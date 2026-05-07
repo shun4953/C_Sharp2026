9a74fee4d0f79d7a85dfaa4dce7cf4a9d04efff0
GAME\262j011 <shanbenjunnaichng@gmail.com>
2026-05-07 13:58:12 +0900

    すべてできています 感想：累乗が使えないのは想定外だった。結局ごり押しをつかった

---
+++ C_Sharp2026/C_Sharp2026/InputProcess.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp2026
+{
+    internal class HogeHoge
+    {
+        private static void Main(string[] args)
+        {
+            /*
+            Console.WriteLine("名前を入力してください");
+            string name = Console.ReadLine();
+            Console.WriteLine("私の名前は{0}です。", name);
+            //
+            Console.WriteLine("足したい数字を入力してください");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            int c = a + b;
+            Console.WriteLine("{0} + {1} = {2}", a, b, c);
+            */
+            Console.WriteLine("自身の体重を入力してください");
+            int weight = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine("自身の身長を入力してください");
+            int heightα = int.Parse(Console.ReadLine());
+            
+            float heightβ = heightα / 100.0f;
+            float bmi = weight / (heightβ * heightβ);
+            Console.WriteLine("あなたのBMI値は{0}です。", bmi);
+            
+
+
+        }
+    }
+}
---
+++ C_Sharp2026/C_Sharp2026/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*
+Console.WriteLine("Hello, World!");
+Console.WriteLine("こんにちは!");
+Console.WriteLine("＼(^o^)／ｵﾜﾀ、うっああうあぁぁぁ");
+
+int a = 0;
+int b = 156;
+int c = 100000000;
+
+float d = 0.0f;
+float e = 12.589f;
+float f = 10000000000000.0f;
+
+String g = "";
+String h = "hello";
+String i = "こんにちは";
+
+bool j = true;
+bool k = false;
+*/
+
+//やあ
+//わたしだ
+//↑だれだよ
